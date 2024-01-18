7ba90eee8c7c5e7875716dc745009660dc1c6013
muh1ddin <rmamuhit@gmail.com>
2024-01-18 21:51:00 +0500

    NEW

---
+++ hmw/lesson.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() { /*
+
+		var name, work, k string
+		var age int
+		fmt.Println("enter your name")
+		_, err := fmt.Scan(&name)
+		fmt.Println(err)
+
+		fmt.Println("what is your age ")
+
+		fmt.Scan(&age)
+		fmt.Println("your age is")
+
+		fmt.Println("where you are working ")
+
+		fmt.Scan(&work)
+
+		fmt.Println(err)
+
+		fmt.Printf("my name %s,my age%d,my work%s\n", name, age, work)
+
+		k = fmt.Sprint(k)
+
+	*/
+	fmt.Println("what is your age ")
+
+}
